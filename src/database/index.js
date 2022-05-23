e07a7a3d1a10a71ad5f33c683dc41390c7b557fb
julbrek13 <julianmacarron@hotmail.com>
2022-05-23 09:28:00 -0300

    pongo la url y el nombre de la bd en donde correspÃonde

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,8 @@ const { MongoClient } = require("mongodb");
 //tarigo el paquete de debug pára imprimir lo que necesite, con otro parametro y el nombre del modulo en el que me encuentro
 const debug = require("debug")("app:module-database");
 
+const { Config } = require("../config/index");
+
 //el archivo exportara una funcion que se encargara de devolver la conexiona la bd.
 //inicializo la variable en nula
 var connection = null;
@@ -15,14 +17,16 @@ module.exports.Database = (collection) =>
       if (!connection) {
         //genero un cliente
         //mongo client devuelve una conexion
-        const cliente = new MongoClient();
+        const cliente = new MongoClient(Config.mongoUri);
         let connection = await cliente.connect();
         debug("nueva conexion realizada con mongoaDB Atlas");
       }
       debug("Reutilizando conexion");
       //en el caso que ya este creada creo una variable y almaceno dicha conexion
-      const db = connection.db();
+      const db = connection.db(Config.mongoDbname);
       //si todo va bien devuelve...
       resolve(db.collection(collection));
-    } catch (error) {}
+    } catch (error) {
+      reject(error);
+    }
   });
